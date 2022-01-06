5df82246a9e5b07ef60fb0f96d4edfebec140e66
KrutaCode <61935060+KrutaCode@users.noreply.github.com>
2022-01-06 10:49:35 -0800

    Update commodities.py

--- Commodities/commodities.py
+++ Commodities/commodities.py
@@ -1,9 +1,12 @@
 import gspread
+import time
 import requests
 import yahoo_fin.stock_info as si
 from bs4 import BeautifulSoup
+import pickle
 from lxml import etree
 from oauth2client.service_account import ServiceAccountCredentials
+from gspread.exceptions import APIError
 
 #A #B #C #D #E #F #G #H #I #J #K #L #M #N #O #P #Q #R #S #T #U #V #W #X #Y #Z
 #1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
@@ -22,6 +25,7 @@ class Stock:
     def __init__(self,ticker):
         #Assign ticker to class member
         self.ticker = ticker
+        self.error_count = 0
 
         #Assemble url
         self.url = f"https://finance.yahoo.com/quote/{self.ticker}?p={self.ticker}&.tsrc=fin-srch"
@@ -72,29 +76,12 @@ class Stock:
 
 class Commodities:
     def __init__(self):
-        self.sheet = client.open("Market Overview").sheet1
-        commodity = {"Oil WTI":"MCL=F",
-                     "Oil Brent":"BZ=F",
-                     "Natural Gas":"NG=F",
-                     "Gold":"GC=F",
-                     "Silver":"SI=F",
-                     "Copper":"HG=F",
-                     "Palladium":"PA=F",
-                     "Platinum":"PL=F",
-                     "Aluminum":"ALI=F",
-                     "Wheat US":"ZW=F",
-                     "Steel":"^STEEL",
-                     "Hot-Rolled Coils":"HRC=F",
-                     "Sugar US":"SB=F",
-                     "Coffee US":"KC=F",
-                     "Cotton US":"CT=F",
-                     "Cocoa US":"CC=F",
-                     "Live Cattle":"LE=F",
-                     "Lumber":"LBS=F"}
-        #Gets the items of the dictionary
-        dictionary_items = commodity.items()
+        self.sheet = client.open("Market Screener").sheet1
+        commodity = self.loadFromDB()
+        self.error_count = 0
+
         # Sorts the dictionary
-        self.commodities = sorted(dictionary_items)
+        self.commodities = sorted(commodity)
 
     ##########################
     # Function: updateSpreadsheet()
@@ -114,23 +101,40 @@ class Commodities:
 
         for key,val in self.commodities:
             url = f"https://finance.yahoo.com/quote/{val}?p={val}&.tsrc=fin-srch"
-            #Updates the cells the with the name and hyperlink.
-            names_cell_value = self.sheet.cell(row,names_col).value
-            if names_cell_value == None:
-                self.sheet.update_cell(row, names_col, f'=HYPERLINK("{url}","{key}")')
+
+            # Create stock object
+            s = Stock(val)
+            # Get the price
+            price = s.getPrice()
+            # Get the percent change
+            pctChange = s.getPctChange()
+
+            #Gets the value of the cells
+            names_cell_value = self.sheet.cell(row, names_col).value
+            price_cell_value = self.sheet.cell(row, price_col).value
+            pctChange_cell_value = self.sheet.cell(row,pctChance_col).value
+
+            # If the cell is empty or the value in it does not match the new value
+            if names_cell_value == None or names_cell_value != key:
+                # Updates the cells the with the name and hyperlink.
+                self.updateCell(row, names_col, f'=HYPERLINK("{url}","{key}")')
             else:
                 pass
-            #Create stock object
-            s = Stock(val)
-            #Get the price
-            price = s.getPrice()
-            #Get the percent change
-            pctChange = s.getPctChange()
-            #Update the price
-            self.sheet.update_cell(row, price_col, price)
-            #Update the percent change
-            self.sheet.update_cell(row, pctChance_col, pctChange)
+            # If the cell is empty or the value in it does not match the new value
+            if price_cell_value == None or price_cell_value != price:
+                #Update the price
+                self.updateCell(row, price_col, price)
+            else:
+                pass
+
+            # If the cell is empty or the value in it does not match the new value
+            if pctChange_cell_value == None or pctChange_cell_value != pctChange:
+                #Update the percent change
+                self.updateCell(row, pctChance_col, pctChange)
+            else:
+                pass
             row += 1
+        print(f"---Finished updating the spreadsheet---")
     ##########################
     # Function: formatNumber()
     # Description: Returns a number formatted like a currency.
@@ -139,7 +143,59 @@ class Commodities:
     def formatNumber(self,num):
         formattedNum = "{:,.2f}".format(num)
         return formattedNum
+    ##########################
+    # Function: updateCell(int,int,string)
+    # Description: Will attempt to update the cell at the specified coordinates with the data passed in the parameter.
+    # Returns: None
+    ##########################
+    def updateCell(self,row,col,data):
+        try:
+            self.sheet.update_cell(row,col,data)
+        except APIError:
+            time.sleep(90)
+            print(f"---Quote exceeded...   Attempt: {self.error_count}---")
+            self.error_count += 1
+            if self.error_count >= 5:
+                self.error_count = 0
+                return
+            else:
+                self.updateCell(row,col,data)
 
+    ##########################
+    # Function: addToDB()
+    # Description: Adds item to pickle file
+    # Returns: None
+    ##########################
+    def addToDB(self, name, ticker):
+        file = open("commodities.pkl", "wb")
+        self.commodities.append((name, ticker))
+        pickle.dump(self.commodities, file)
+        file.close()
+        print(f"---{ticker} was successfully added to the database---")
+    ##########################
+    # Function: loadFromDB()
+    # Description: Loads data from a pickle file into a variable.
+    # Returns: list of tuples
+    ##########################
+    def loadFromDB(self):
+        file = open("commodities.pkl","rb")
+        data = pickle.load(file)
+        file.close()
+        return data
+    def removeFromDB(self,ticker):
+        data = self.loadFromDB()
+        new_data = []
+        index = 0
+        print(f"First:")
+        for key,val in data:
+            if val == ticker:
+                del data[index]
+            print(f"{val}")
+            index += 1
+
+        file = open("commodities.pkl","wb")
+        pickle.dump(data,file)
+        file.close()
 
 def main():
     c = Commodities()
